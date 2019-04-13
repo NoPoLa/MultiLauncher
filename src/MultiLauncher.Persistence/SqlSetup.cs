7c705825340a4cb2a615059cb7107f3f40066476
Frej <frej.fredholm@gmail.com>
2019-04-13 15:37:09 +0200

    Added UI.Test and Presistence project

---
+++ src/MultiLauncher.Persistence/SqlSetup.cs
@@ -0,0 +1,12 @@
+﻿//using System;
+//using System.Data.SQLite;
+//using System.IO;
+//using System.Threading.Tasks;
+
+//namespace MultiLauncher.Persistence
+//{
+//    public class SqlSetup
+//    {
+//        
+//    }
+//}
